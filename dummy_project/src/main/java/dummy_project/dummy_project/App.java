7c295329e3a0928a43f3e0a49063cbb176f81dd5
John Eifert <johnreifert94@gmail.com>
2018-05-02 13:16:00 -0400

    adding dummy_project, which is a maven project

---
+++ dummy_project/src/main/java/dummy_project/dummy_project/App.java
@@ -0,0 +1,16 @@
+package dummy_project.dummy_project;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
