94988f86eeaa5ec1f77f639dcb8634a9e37c61f2
John Eifert <johnreifert94@gmail.com>
2018-05-02 13:49:07 -0400

    trying again

--- dummy_project/src/main/java/dummy_project/DummyProject.java
+++ dummy_project/src/main/java/dummy_project/DummyProject.java
@@ -1,11 +1,18 @@
 //John Eifert
 package dummy_project;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
-public class DummyProject
+
+public class DummyProject extends HttpServlet
 {
-	public static void main(String args[])
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
 	{
 		System.out.println("Hello, world!");
 	}
